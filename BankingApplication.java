66d4d54b1816a49c35092082d3f58be1ff867075
Sindhuja <49586403+sindhujamankala@users.noreply.github.com>
2022-05-23 15:36:30 +0530

    Update BankingApplication.java

--- BankingApplication.java
+++ BankingApplication.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class BankingApplication{
     public static void main(String[]args){
-        // TODO Auto-generated method stub
         BankAccount obj1= new BankAccount("XYZ","BA0001"); 
         obj1.showMenu();
    }
